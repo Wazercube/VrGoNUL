4f5eb66e079f2881fdecbca901dd899eaa9e798b
Wazercube <wazercube_34@outlook.fr>
2019-06-04 10:45:17 +0200

    Ajouts scripts

---
+++ Assets/Script/BonusToRecover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BonusType { WEAPON, RACKET, BALL};
+
+public class BonusToRecover : MonoBehaviour
+{
+
+    public BonusType type;
+    [Range(0, 1)]
+    public float rangeX;
+    [Range(0, 1)]
+    public float rangeY;
+
+    [Range(0.01f,5)]
+    public float speed = 0.01f;
+    private GameObject Player;
+
+    private Vector3 positionToReach;
+
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        Player = GameObject.FindGameObjectWithTag("Player");
+        positionToReach = Player.transform.position + new Vector3(Random.Range(-rangeX, rangeX), Random.Range(-rangeY, rangeY));
+        Debug.Log(positionToReach);
+        direction = positionToReach - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * (speed) * Time.deltaTime ;
+    }
+}
