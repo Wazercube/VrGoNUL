b114879a9b8adbd2113dd2a362fa83c0be4c07c7
Wazercube <wazercube_34@outlook.fr>
2019-06-04 10:29:00 +0200

    Init

---
+++ Assets/Script/ViewScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewScript : MonoBehaviour
+{
+    public LayerMask mask;
+    public GameObject panier;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, mask))
+        {
+            panier.transform.position = hit.point;
+            
+            Debug.Log(hit.point);
+        }
+    }
+}
